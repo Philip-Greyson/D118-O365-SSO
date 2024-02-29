8891005567df164721ecf42e88ae14e2d6f90c42
Philip Greyson <pgreyson@d118.org>
2024-02-29 13:57:11 -0600

    fixed stupid typos in powershell script, added more comments so I remember what everything does

--- Get-immutable-IDs.ps1
+++ Get-immutable-IDs.ps1
@@ -1,11 +1,10 @@
-$OutputFileName = "Immutable-Ids.csv"
-$OutputPath= Join-Path $PSScriptRoot $OUTPUT_FILE_NAME
+$OutputFileName = "Immutable-Ids.csv"  # Name of the output file
+$OutputPath= Join-Path $PSScriptRoot $OutputFileName  # Construct the output path string by taking the current directory and appending the output file name
 
 # Office 365 Credentials
-$O365UserName = $Env:O365_USERNAME
-$0365Password = $Env:O365_PASSWORD | ConvertTo-SecureString
-#$O365Password = Get-Content C:\D118-O365-SSO\O365-Pass.txt | ConvertTo-SecureString
-$O365Credentials = New-Object System.Management.Automation.PSCredential ($O365UserName, $O365Password)
-Connect-MsolService -Credential $O365Credentials
+$O365UserName = $Env:O365_USERNAME  # Get the O365 username from the environment variable
+$O365Password = $Env:O365_PASSWORD | ConvertTo-SecureString -AsPlainText -Force  # Get the 065 password from the environment variable, convert it from plaintext to securestring
+$O365Credentials = New-Object System.Management.Automation.PSCredential ($O365UserName, $O365Password)  # Create the O365 credentials object
+Connect-MsolService -Credential $O365Credentials  # Connect to o365/Azure AD - whatever they want to call it now
 # Do the export
-$exportUsers = Get-MsolUser -All | Select-Object UserprincipalName, ImmutableID | Export-Csv $OutputPath
+Get-MsolUser -All | Select-Object UserprincipalName, ImmutableID | Export-Csv $OutputPath  # Export the UPN and immutible ID fields for all users and export them to our output path
